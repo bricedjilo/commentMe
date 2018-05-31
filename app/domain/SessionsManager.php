a0bd455a4dfa45f2645b5901136a4e88a9965676
Brice Djilo <dbrice@uno.edu>
2018-05-31 15:25:23 -0600

    fixed login error message

--- app/domain/SessionsManager.php
+++ app/domain/SessionsManager.php
@@ -15,10 +15,13 @@ class SessionsManager {
         if ( empty(array_filter($users)) )
         {
             throw new CustomException(CustomExceptionType::AUTH, "Incorrect email or password. Try again.");
-        } elseif ( !empty(array_filter($users)) && Utility::verify($password, $users[0]->getPassword()) )
-        {
-            App::get('session')->set(["user" => $users[0]]);
-            return App::get('session');
+        } elseif ( count(array_filter($users)) === 1 ) {
+            if( Utility::verify($password, $users[0]->getPassword()) ) {
+                App::get('session')->set(["user" => $users[0]]);
+                return App::get('session');
+            } else {
+                throw new CustomException(CustomExceptionType::AUTH, "Incorrect email or password. Try again.");
+            }
         } else {
             throw new CustomException(CustomExceptionType::SQL_CONSTRAINT,
                 "Oops .. Something went wrong. We could not log you in.");
